cc05655f47fd07f8e7d4fce1aeedc08c0a3ff599
Mohammed Shaco <mohammedshaco@gmail.com>
2024-04-20 19:11:06 +0200

    Create the route

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
