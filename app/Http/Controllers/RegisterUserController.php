bb0d3866165a92b2ae67207953f5552565028e3b
Mohammed Shaco <mohammedshaco@gmail.com>
2024-05-05 08:35:36 +0200

    create RegisterUserController that will handle the create a new user and log him in

---
+++ app/Http/Controllers/RegisterUserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class RegisterUserController extends Controller
+{
+    // Show register form
+    public function create() {
+        return view('auth.register');
+    }
+
+    // Save the data into the database
+    public function store(Request $reqeust) {
+        // Validate the data
+        $userInformation = $reqeust->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'max:254', 'unique: User,email'],
+            'password' => ['required', 'min:8' , 'confirmed']
+        ]);
+
+        // create a user
+        $user = User::create($userInformation);
+
+        // log him in
+        auth::login($user);
+
+        // redirct the user
+        return redirect('/dashboard');
+    }
+}
